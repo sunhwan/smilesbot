606dc4b71f5fe363696f19151ded423ad256e9cf
Sunhwan Jo <sunhwanj@gmail.com>
2019-03-13 11:55:15 -0700

    avoide unicode encoding error/improve drawing quality

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,19 @@
-import os
-from rdkit import Chem
-from rdkit.Chem import Draw
-from io import BytesIO
-
 from flask import Flask
 from flask import send_file
+
+import os
+from io import BytesIO
+
+# avoid unicode encoding error
+os.environ['RDK_NOPANGO'] = "1"
+
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw.MolDrawing import MolDrawing, DrawingOptions 
+
+DrawingOptions.atomLabelFontSize = 55
+DrawingOptions.dotsPerAngstrom = 100
+DrawingOptions.bondLineWidth = 3.0
 
 app = Flask(__name__)
 
